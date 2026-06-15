1c9333f4ea5c837eb21cc13ef514c49cda671d5e
Tsukioka1020 <fg1020saata@gmail.com>
2026-06-16 00:26:19 +0900

    dreamsのCRUD実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-    get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
-    root "home#index"
-  end
+  devise_for :users
+
+  resources :dreams
+
+  root "home#index"
+end
