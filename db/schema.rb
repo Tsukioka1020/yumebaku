a10a9d96b03232d0997d08c952c41b4403c20d7b
Tsukioka1020 <fg1020saata@gmail.com>
2026-06-30 22:27:17 +0900

    feat: 初回ログインガイドモーダル実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_07_114134) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_30_114052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_07_114134) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "first_login", default: true
     t.string "name"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
